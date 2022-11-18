f9d5a1a2b900a6f18cd013fd607d69201f2b3fd0
jeezft <74239828+jeezft@users.noreply.github.com>
2022-11-18 10:10:48 +0300

    feat: simple hello world

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/xxdstem/gatari-go-api/handlers"
+)
 
+func main() {
+	f := fiber.New()
+
+	api := f.Group("/api/v1")
+
+	api.Get("/hello", handlers.Hello)
+
+	f.Listen(":8080")
 }
